caf45b2fcbcc79078d8a06d9d55c6fcbca4d4512
Vittal <vittalp88@gmail.com>
2017-01-31 18:31:43 -0500

    have path for data dir

--- tf_image_segmentation/utils/set_paths.py
+++ tf_image_segmentation/utils/set_paths.py
@@ -3,9 +3,6 @@ import sys
 import os
 import socket
 
-# Use second GPU -- change if you want to use a first one
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-
 flags = tf.app.flags
 
 machine = socket.gethostname()
@@ -15,13 +12,17 @@ if machine == 'ccvl-4gpu':
     # Get it from here:
     # https://github.com/warmspringwinds/models/tree/fully_conv_vgg
 
+    # Use second GPU -- change if you want to use a first one
+    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
     flags.DEFINE_string("slim_path", "/home/vittal/work/tf-slim-models/models/slim", "The path to tf slim repo")
 
     # Add path to the cloned library
     flags.DEFINE_string("tf_image_seg_dir", "/home/vittal/work/segmentation/tf-image-segmentation/", "Dir for tf-image-segmentation repo")
-    flags.DEFINE_string("checkpoints_dir", "/home/vittal/work/ckpts/", "Directory where checkpoints are saved")
+    flags.DEFINE_string("checkpoints_dir", "/home/vittal/work/ckpts/", "Directory where ImageNet pretrained checkpoints are saved")
     flags.DEFINE_string("log_dir", "/home/vittal/work/segmentation/tf-image-segmentation/log_dir/", "Directory to save TF logs")
-    flags.DEFINE_string("save_dir", "/home/vittal/work/segmentation/tf-image-segmentation/save_dir/", "Directory to save checkpoint models")
+    flags.DEFINE_string("save_dir", "/home/vittal/work/segmentation/tf-image-segmentation/save_dir/", "Directory to save trained models")
+    flags.DEFINE_string("data_dir", "/home/vittal/work/segmentation/tf-image-segmentation/data/", "Directory which hosts datasets")
 
 elif 'login' or 'gpu' in machine:
     # Add a path to a custom fork of TF-Slim
@@ -32,9 +33,10 @@ elif 'login' or 'gpu' in machine:
 
     # Add path to the cloned library
     flags.DEFINE_string("tf_image_seg_dir", "/home-4/vpremac1@jhu.edu/projects/tf-image-segmentation/", "Dir for tf-image-segmentation repo")
-    flags.DEFINE_string("checkpoints_dir", "/home-4/vpremac1@jhu.edu/scratch/ckpts/", "Directory where checkpoints are saved")
+    flags.DEFINE_string("checkpoints_dir", "/home-4/vpremac1@jhu.edu/scratch/ckpts/", "Directory where ImageNet pretrained checkpoints are saved")
     flags.DEFINE_string("log_dir", "/home-4/vpremac1@jhu.edu/projects/tf-image-segmentation/tf_image_segmentation/log_dir/", "Directory to save TF logs")
-    flags.DEFINE_string("save_dir", "/home-4/vpremac1@jhu.edu/projects/tf-image-segmentation/tf_image_segmentation/save_dir/", "Directory to save checkpoint models")
+    flags.DEFINE_string("save_dir", "/home-4/vpremac1@jhu.edu/projects/tf-image-segmentation/tf_image_segmentation/save_dir/", "Directory to save trained models")
+    flags.DEFINE_string("data_dir", "/home-4/vpremac1@jhu.edu/projects/tf-image-segmentation/tf_image_segmentation/data/", "Directory which hosts datasets")
 
 elif "thin6" in machine:
     # Add a path to a custom fork of TF-Slim
@@ -53,3 +55,7 @@ FLAGS = flags.FLAGS
 
 sys.path.append(FLAGS.slim_path)
 sys.path.append(FLAGS.tf_image_seg_dir)
+
+if not os.path.exists(FLAGS.data_dir):
+    print('Could not find path to datasets. Exiting...')
+    sys.exit()
